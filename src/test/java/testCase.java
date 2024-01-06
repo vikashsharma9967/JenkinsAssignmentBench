1c5d846cb66d88f6574af294dc6b94a0f3ea18b2
VikashSharma03 <vikas.sharma@tungsten-network.com>
2024-01-06 10:54:28 +0530

    first commit

---
+++ src/test/java/testCase.java
@@ -0,0 +1,21 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class testCase {
+
+    @Test
+    public void testDemo(){
+        System.out.println("Hello world");
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.amazon.com");
+        System.out.println("Page Title :- "+ driver.getTitle());
+        System.out.println("Website launch successfully");
+        driver.close();
+    }
+}
